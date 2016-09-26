eba4c3c0fd1c522132c8f85a70ef75e390de3828
Rowan Cockett <rowan@3ptscience.com>
2016-09-26 11:14:47 -0600

    Add setup.py

--- README.md
+++ README.md
@@ -1,2 +1,28 @@
-# vectormath
-Vector math utilities for python.
+vectormath
+**********
+
+Vector math utilities for Python.
+
+```python
+import numpy as np
+import vectormath as vmath
+
+# Simple vectors
+v = vmath.Vector3(5, 0, 0)
+v.normalize()
+print(v) # > [[1, 0, 0]]
+print(v.x, type(v.x))  # > 1.0, float
+
+# Array based vectors are much faster than a for loop
+v_array = vmath.Vector3([[4,0,0],[0,2,0]])
+print(v_array.x) # > [4, 0]
+# we can
+print(v_array.length) # [4, 2]
+print(v_array.normalize())  # [[1,0,0],[0,1,0]]
+
+# These vectors are just numpy arrays
+assert isinstance(v, np.ndarray)
+```
+
+
+Major classes include `Vector3`, `Matrix3`, `Plane`, `Parallelogram`.
---
+++ setup.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python
+'''
+    steno3d: Client library for Steno3D & steno3d.com
+'''
+
+from distutils.core import setup
+from setuptools import find_packages
+
+CLASSIFIERS = [
+    'Development Status :: 4 - Beta',
+    'Programming Language :: Python',
+    'Topic :: Scientific/Engineering',
+    'Topic :: Scientific/Engineering :: Mathematics',
+    'Topic :: Scientific/Engineering :: Physics',
+    'Operating System :: Microsoft :: Windows',
+    'Operating System :: POSIX',
+    'Operating System :: Unix',
+    'Operating System :: MacOS',
+    'Natural Language :: English',
+]
+
+with open('README.md') as f:
+    LONG_DESCRIPTION = ''.join(f.readlines())
+
+setup(
+    name='vectormath',
+    version='0.0.1',
+    packages=find_packages(),
+    install_requires=[
+        'future',
+        'six',
+        'numpy>=1.7',
+    ],
+    author='3point Science',
+    author_email='info@3ptscience.com',
+    description='vectormath',
+    long_description=LONG_DESCRIPTION,
+    keywords='linear algebra, vector, plane, math',
+    url='https://github.com/3ptscience/vectormath',
+    download_url='http://github.com/3ptscience/vectormath',
+    classifiers=CLASSIFIERS,
+    platforms=['Windows', 'Linux', 'Solaris', 'Mac OS-X', 'Unix'],
+    license='MIT License',
+    use_2to3=False,
+)
