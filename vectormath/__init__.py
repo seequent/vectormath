e7fe957fcab8aa38b304e295d184fb1f20c8cc34
Lindsey Heagy <lindseyheagy@gmail.com>
2016-09-25 17:58:04 -0700

    use python3 compatible imports

--- vectormath/__init__.py
+++ vectormath/__init__.py
@@ -1,2 +1,2 @@
-from vector import Vector3
-from matutils import mkvc, ndgrid, ouv2vec, switchOUVZ, transformOUV
+from .vector import Vector3
+from .matutils import mkvc, ndgrid, ouv2vec, switchOUVZ, transformOUV
