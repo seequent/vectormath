01b16fae3701b16dbabb0b51d751ab7b12865e2a
fwkoch <franklin@3ptscience.com>
2017-07-17 15:36:59 -0600

    Bump version: 0.1.2 → 0.1.3

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ with open('README.rst') as f:
 
 setup(
     name='vectormath',
-    version='0.1.2',
+    version='0.1.3',
     packages=find_packages(),
     install_requires=[
         'numpy>=1.7',
