08b59748bba10d51eec1a630d8f4cf09892e397f
Franklin Koch <franklin.koch@seequent.com>
2018-04-16 00:25:29 -0600

    Remove pyexec dependency

--- docs/conf.py
+++ docs/conf.py
@@ -34,7 +34,6 @@ extensions = [
     'sphinx.ext.intersphinx',
     'sphinx.ext.mathjax',
     'sphinx.ext.viewcode',
-    'pyexec'
 ]
 
 # Add any paths that contain templates here, relative to this directory.
