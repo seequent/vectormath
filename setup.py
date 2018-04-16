b3e6aad9ee50aa5d8b1cb3f67f2a00983090401c
Franklin Koch <franklin.koch@seequent.com>
2018-04-16 01:10:22 -0600

    Bump version: 0.1.4 → 0.2.0

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ with open('README.rst') as f:
 
 setup(
     name='vectormath',
-    version='0.1.4',
+    version='0.2.0',
     packages=find_packages(),
     install_requires=[
         'numpy>=1.7',
--- vectormath/__init__.py
+++ vectormath/__init__.py
@@ -6,7 +6,7 @@ from __future__ import unicode_literals
 
 from .vector import Vector3, Vector2, Vector3Array, Vector2Array
 
-__version__ = '0.1.4'
+__version__ = '0.2.0'
 __author__ = 'Seequent'
 __license__ = 'MIT'
 __copyright__ = 'Copyright 2018 Seequent'
