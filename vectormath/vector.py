f64065bc716bbb924aa576072f2ad23a4b33268c
Joseph Contreras <26684136+JosephJContreras@users.noreply.github.com>
2019-01-05 15:04:30 -0600

    Add angle method to calculate angle between two Vectors (issue 31)
    The angle method will calculate the angle between two
    Vectors (both Vector2 or Vector3) and return the angle
    either radians (default) or degrees
    
    This is to address the wishlish requested in issue 31

--- vectormath/vector.py
+++ vectormath/vector.py
@@ -85,6 +85,21 @@ class BaseVector(np.ndarray):
             raise TypeError('Cross product operand must be a vector')
         return self.__class__(np.cross(self, vec))
 
+    def angle(self, vec, unit='rad'):
+        """Angle between two vectors given in either radians ('rad') or degrees
+        ('deg'). Default to radians
+        """
+        if not isinstance(vec, self.__class__):
+            raise TypeError('Angle operand must be a vector')
+        if unit not in ['deg', 'rad']:
+            raise ValueError('Only units of rad or deg are supported')
+
+        ang = np.arccos(self.dot(vec) / (self.length * vec.length))
+        if unit == 'rad':
+            return ang
+        else:
+            return ang * 180 / np.pi
+
     def __mul__(self, multiplier):
         return self.__class__(self.view(np.ndarray) * multiplier)
 
@@ -288,6 +303,10 @@ class BaseVectorArray(BaseVector):
             raise ValueError('Dot product operands must have the same '
                              'number of elements.')
         return np.sum((getattr(self, d)*getattr(vec, d) for d in self.dims), 1)
+
+    def angle(self, vec=None):
+        """Angle method is only for Vectors, not VectorArrays"""
+        raise NotImplementedError('angle not implemented for VectorArrays')
 
 
 class Vector3Array(BaseVectorArray):
