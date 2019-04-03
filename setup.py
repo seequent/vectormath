6bdfc18a494736431d2b4d75a3c6586b0ed3f732
Franklin Koch <franklin.koch@seequent.com>
2019-04-03 14:11:58 -0600

    Bump version: 0.2.1 → 0.2.2

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ with open('README.rst') as f:
 
 setup(
     name='vectormath',
-    version='0.2.1',
+    version='0.2.2',
     packages=find_packages(),
     install_requires=[
         'numpy>=1.7',
